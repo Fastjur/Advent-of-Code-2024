f076cdce3bfabdac4134d5e61621d364d0b5dbd7
Jurriaan Den Toonder <1493561+Fastjur@users.noreply.github.com>
2024-12-08 19:52:32 +0100

    Made Matrix helper class

---
+++ structures/Matrix.ts
@@ -0,0 +1,126 @@
+// @ts-types="npm:@types/lodash"
+import _ from "npm:lodash";
+
+export interface IMatrix<T> {
+  numRows: number;
+  numColumns: number;
+  isSquare: boolean;
+  rows: Iterable<T[]>;
+  columns: Iterable<T[]>;
+  diagonals: Iterable<T[]>;
+  get(row: number, col: number): T;
+  set(row: number, col: number, value: T): void;
+  getRow(row: number): T[];
+  getColumn(col: number): T[];
+  getDiagonal(row: number, col: number): T[];
+  transpose(): IMatrix<T>;
+  mirrorVertically(): IMatrix<T>;
+  mirrorHorizontally(): IMatrix<T>;
+  // TODO
+  // rotateClockwise(): IMatrix<T>;
+  // rotateCounterClockwise(): IMatrix<T>;
+}
+
+export class Matrix<T> implements IMatrix<T> {
+  private readonly data: T[][];
+  public readonly numRows: number;
+  public readonly numColumns: number;
+  public readonly isSquare: boolean;
+
+  constructor(data: T[][]) {
+    if (data.length === 0) {
+      throw new Error("Matrix must have at least one row!");
+    }
+    this.numRows = data.length;
+    this.numColumns = data[0].length;
+    // Check that every row has the same number of columns
+    if (!data.every((row) => row.length === data[0].length)) {
+      throw new Error("All rows must have the same number of columns!");
+    }
+    this.isSquare = data.length === data[0].length;
+    this.data = _.cloneDeep(data);
+  }
+
+  static ofSize<T>(
+    rows: number,
+    columns: number,
+    value: T | undefined,
+  ): Matrix<T | undefined> {
+    const data = Array.from(
+      { length: rows },
+      () => Array.from({ length: columns }, () => value),
+    );
+    return new Matrix(data);
+  }
+
+  get rows(): Iterable<T[]> {
+    return this.data;
+  }
+
+  get columns(): Iterable<T[]> {
+    return this.data[0].map((_, i) => this.data.map((row) => row[i]));
+  }
+
+  get diagonals(): Iterable<T[]> {
+    if (!this.isSquare) {
+      // TODO, for now we only support square matrices
+      throw new Error("Matrix must be square to get its diagonals!");
+    }
+    const diagonals = [];
+    for (let i = 0; i < Math.min(this.numColumns, this.numRows); i++) {
+      diagonals.push(this.getDiagonal(0, i));
+      if (i > 0) {
+        diagonals.push(this.getDiagonal(i, 0));
+      }
+    }
+    return diagonals;
+  }
+
+  get(row: number, col: number): T {
+    if (row >= this.numRows) {
+      throw new Error("Row index out of bounds!");
+    }
+    if (col >= this.numColumns) {
+      throw new Error("Column index out of bounds!");
+    }
+    return this.data[row][col];
+  }
+
+  set(row: number, col: number, value: T): void {
+    this.data[row][col] = value;
+  }
+
+  getRow(row: number): T[] {
+    return this.data[row];
+  }
+
+  getColumn(col: number): T[] {
+    return this.data.map((row) => row[col]);
+  }
+
+  getDiagonal(row: number, col: number): T[] {
+    const diagonal = [];
+    for (let i = 0; i < Math.min(this.numColumns, this.numRows); i++) {
+      if (row + i >= this.numRows || col + i >= this.numColumns) {
+        break;
+      }
+      diagonal.push(this.data[row + i][col + i]);
+    }
+    return diagonal;
+  }
+
+  transpose(): Matrix<T> {
+    const transposed = this.data[0].map((_, i) =>
+      this.data.map((row) => row[i])
+    );
+    return new Matrix(transposed);
+  }
+
+  mirrorHorizontally(): Matrix<T> {
+    return new Matrix(this.data.map((row) => row.slice().reverse()));
+  }
+
+  mirrorVertically(): IMatrix<T> {
+    return new Matrix(this.data.slice().reverse());
+  }
+}
---
+++ structures/tests/Matrix.test.ts
@@ -0,0 +1,179 @@
+import { expect } from "jsr:@std/expect";
+
+// @ts-types="npm:@types/lodash"
+import _ from "npm:lodash";
+import { Matrix } from "../Matrix.ts";
+
+Deno.test("MatrixTest", async (t) => {
+  const defaultMatrix = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+    ["g", "h", "i"],
+  ];
+
+  const nonSquareMatrix = [
+    ["a", "b", "c"],
+    ["d", "e", "f"],
+  ];
+
+  await t.step("Matrix creation with default data", () => {
+    const data = _.cloneDeep(defaultMatrix);
+    const matrix = new Matrix(data);
+    expect(matrix.numRows).toBe(3);
+    expect(matrix.numColumns).toBe(3);
+    expect(matrix.isSquare).toBe(true);
+    expect(matrix.get(0, 0)).toBe("a");
+    expect(matrix.get(1, 1)).toBe("e");
+    expect(matrix.get(2, 2)).toBe("i");
+  });
+
+  await t.step("Matrix creation with non-square data", () => {
+    const matrix = new Matrix(_.cloneDeep(nonSquareMatrix));
+    expect(matrix.numRows).toBe(2);
+    expect(matrix.numColumns).toBe(3);
+    expect(matrix.isSquare).toBe(false);
+    expect(matrix.get(0, 0)).toBe("a");
+    expect(matrix.get(1, 1)).toBe("e");
+  });
+
+  await t.step("Matrix creation with empty data", () => {
+    expect(() => new Matrix([])).toThrow("Matrix must have at least one row");
+  });
+
+  await t.step(
+    "Should throw an error if rows have different number of columns",
+    () => {
+      const data = [
+        ["a", "b", "c"],
+        ["d", "e"],
+      ];
+      expect(() => new Matrix(data)).toThrow(
+        "All rows must have the same number of columns!",
+      );
+    },
+  );
+
+  await t.step("Should return the rows iterator", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const rows = Array.from(matrix.rows);
+    expect(rows).toEqual(defaultMatrix);
+  });
+
+  await t.step("Should return the columns iterator", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const columns = Array.from(matrix.columns);
+    expect(columns).toEqual([
+      ["a", "d", "g"],
+      ["b", "e", "h"],
+      ["c", "f", "i"],
+    ]);
+  });
+
+  await t.step("Should return the diagonals iterator", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const diagonals = Array.from(matrix.diagonals);
+    expect(diagonals).toEqual([
+      ["a", "e", "i"],
+      ["b", "f"],
+      ["d", "h"],
+      ["c"],
+      ["g"],
+    ]);
+  });
+
+  await t.step("Should get the correct value at a specific position", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    expect(matrix.get(0, 0)).toBe("a");
+    expect(matrix.get(1, 1)).toBe("e");
+    expect(matrix.get(2, 2)).toBe("i");
+    expect(matrix.get(0, 2)).toBe("c");
+    expect(matrix.get(2, 0)).toBe("g");
+
+    expect(() => matrix.get(3, 0)).toThrow("Row index out of bounds!");
+    expect(() => matrix.get(0, 3)).toThrow("Column index out of bounds!");
+  });
+
+  await t.step("Should set the correct value at a specific position", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    matrix.set(0, 0, "z");
+    matrix.set(1, 1, "y");
+    matrix.set(2, 2, "x");
+    expect(matrix.get(0, 0)).toBe("z");
+    expect(matrix.get(1, 1)).toBe("y");
+    expect(matrix.get(2, 2)).toBe("x");
+  });
+
+  await t.step("Should get the correct row", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    expect(matrix.getRow(0)).toEqual(["a", "b", "c"]);
+    expect(matrix.getRow(1)).toEqual(["d", "e", "f"]);
+    expect(matrix.getRow(2)).toEqual(["g", "h", "i"]);
+  });
+
+  await t.step("Should get the correct column", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    expect(matrix.getColumn(0)).toEqual(["a", "d", "g"]);
+    expect(matrix.getColumn(1)).toEqual(["b", "e", "h"]);
+    expect(matrix.getColumn(2)).toEqual(["c", "f", "i"]);
+  });
+
+  await t.step("Should get the correct diagonal", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    expect(matrix.getDiagonal(0, 0)).toEqual(["a", "e", "i"]);
+    expect(matrix.getDiagonal(1, 1)).toEqual(["e", "i"]);
+    expect(matrix.getDiagonal(0, 1)).toEqual(["b", "f"]);
+    expect(matrix.getDiagonal(1, 0)).toEqual(["d", "h"]);
+    expect(matrix.getDiagonal(0, 2)).toEqual(["c"]);
+    expect(matrix.getDiagonal(2, 0)).toEqual(["g"]);
+  });
+
+  await t.step("Should transpose the square matrix", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const transposed = matrix.transpose();
+    expect(transposed.numRows).toBe(3);
+    expect(transposed.numColumns).toBe(3);
+    expect(transposed.isSquare).toBe(true);
+
+    expect(transposed.getRow(0)).toEqual(["a", "d", "g"]);
+    expect(transposed.getRow(1)).toEqual(["b", "e", "h"]);
+    expect(transposed.getRow(2)).toEqual(["c", "f", "i"]);
+  });
+
+  await t.step("Should transpose the non-square matrix", () => {
+    const matrix = new Matrix(_.cloneDeep(nonSquareMatrix));
+    const transposed = matrix.transpose();
+    expect(transposed.numRows).toBe(3);
+    expect(transposed.numColumns).toBe(2);
+    expect(transposed.isSquare).toBe(false);
+
+    expect(transposed.getRow(0)).toEqual(["a", "d"]);
+    expect(transposed.getRow(1)).toEqual(["b", "e"]);
+    expect(transposed.getRow(2)).toEqual(["c", "f"]);
+  });
+
+  await t.step("Should mirror the matrix horizontally", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const mirrored = matrix.mirrorHorizontally();
+    expect(mirrored.getRow(0)).toEqual(["c", "b", "a"]);
+    expect(mirrored.getRow(1)).toEqual(["f", "e", "d"]);
+    expect(mirrored.getRow(2)).toEqual(["i", "h", "g"]);
+  });
+
+  await t.step("Should mirror the matrix vertically", () => {
+    const matrix = new Matrix(_.cloneDeep(defaultMatrix));
+    const mirrored = matrix.mirrorVertically();
+    expect(mirrored.getRow(0)).toEqual(["g", "h", "i"]);
+    expect(mirrored.getRow(1)).toEqual(["d", "e", "f"]);
+    expect(mirrored.getRow(2)).toEqual(["a", "b", "c"]);
+  });
+
+  await t.step("Should create a matrix of a specific size", () => {
+    const matrix = Matrix.ofSize(3, 3, "a");
+    expect(matrix.numRows).toBe(3);
+    expect(matrix.numColumns).toBe(3);
+    expect(matrix.isSquare).toBe(true);
+    expect(matrix.get(0, 0)).toBe("a");
+    expect(matrix.get(2, 2)).toBe("a");
+    expect(matrix.get(1, 0)).toBe("a");
+  });
+});
